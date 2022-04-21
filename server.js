8e94add67126a7b9c9fd2cf28ece8d32dcb590a7
Parks Cochrane <pdcochrane@gmail.com>
2022-04-21 11:31:59 -0400

    idek

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const args = require('minimist')(process.argv.slice(2))
-args["port"]
+args['port']
 const port = args.port || process.env.PORT || 5000
 
 const server = app.listen(port, () => {
